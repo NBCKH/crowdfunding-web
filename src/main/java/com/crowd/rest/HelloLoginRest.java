6dd7a9ec415c938bc494caed317da7a0961245e9
chenkaihua <nbchenkh@163.com>
2018-07-12 17:28:38 +0800

    update

---
+++ src/main/java/com/crowd/rest/HelloLoginRest.java
@@ -0,0 +1,15 @@
+package com.crowd.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chenkaihua
+ * @date 2018/7/12 16:40
+ */
+@Slf4j
+@RestController
+@RequestMapping
+public class HelloLoginRest {
+}
